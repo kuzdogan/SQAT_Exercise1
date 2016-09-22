dadd7aa25eec9dcd3fc038885cd066a38a35e7f0
somename <someemail>
2016-09-22 16:11:30 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -19,7 +19,8 @@ public class BowlingGame {
 		
 		// If not the first frame, set the scores.
 		// Here set the nextThrow values of prev frames.
-		if(frame.getFrameNum() > 1){
+		if(frame.isStrike() && 
+				frame.getFrameNum() > 1){
 			Frame prevFrame = frames.get(frame.getFrameNum()-2); // -2 for accessing index starting from 0
 			
 			// If a strike check if double strike.
@@ -29,7 +30,7 @@ public class BowlingGame {
 				if(prevPrevFrame.isStrike()){
 					prevPrevFrame.setNextThrow2(10);
 				} else{
-					prevFrame.setNextThrow1(10);
+					prevFrame.setNextThrow1(frame.);
 				}
 			} else{
 				prevFrame.setNextThrow1(frame.getFirstThrow());
