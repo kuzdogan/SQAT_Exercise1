5998d709dea0324dea0d525e996aa0d2252404c3
somename <someemail>
2016-09-22 15:35:09 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 //Finish time:
 //ID:
@@ -8,6 +10,7 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
+	private Queue<Integer> shotQueue = new LinkedList<Integer>();
 	
 	public BowlingGame(){}
 	
@@ -16,8 +19,14 @@ public class BowlingGame {
 		if (frames.size() >= 10)
 			throw new BowlingException();
 		frame.setFrameNum(frames.size()+1);
+		shotQueue.add(frame.getFirstThrow());
+		shotQueue.add(frame.getSecondThrow());
 		
-		Frame prevFrame = frames.get(frame.getFrameNum()-1);
+		// If not the first frame, set the scores as 
+		if(frame.getFrameNum() != 1){
+			Frame prevFrame = frames.get(frame.getFrameNum()-1);
+		}
+		
 		frames.add(frame);
 	}
 	
