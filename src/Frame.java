ff4a5a088b11fc51e7c9d9506eadb79533e40690
somename <someemail>
2016-09-22 15:17:07 +0300

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -1,14 +1,14 @@
 public class Frame {
 	private int firstThrow;
 	private int secondThrow;
-	
-	public Frame(int firstThrow, int secondThrow) throws BowlingException{
+
+	public Frame(int firstThrow, int secondThrow) throws BowlingException {
 		if (firstThrow + secondThrow > 10)
 			throw new BowlingException();
 		this.firstThrow = firstThrow;
 		this.secondThrow = secondThrow;
 	}
-	
+
 	public int getFirstThrow() {
 		return firstThrow;
 	}
@@ -17,23 +17,30 @@ public class Frame {
 		return secondThrow;
 	}
 
-	//returns the score of a single frame
-	public int score(){
-		//to be implemented
+	// returns the score of a single frame
+	public int score() {
+		if (this.isStrike()) {
+
+		}
+
+		else if (this.isSpare()) {
+
+		}
+		// to be implemented
 		return 0;
 	}
 
-	//returns whether the frame is a strike or not
-	public boolean isStrike(){
-		if(firstThrow == 10)
+	// returns whether the frame is a strike or not
+	public boolean isStrike() {
+		if (firstThrow == 10)
 			return true;
-		//to be implemented
+		// to be implemented
 		return false;
 	}
-	
-	//return whether a frame is a spare or not
-	public boolean isSpare(){
-		if((firstThrow + secondThrow == 10) && firstThrow != 10)
+
+	// return whether a frame is a spare or not
+	public boolean isSpare() {
+		if ((firstThrow + secondThrow == 10) && firstThrow != 10)
 			return true;
 		return false;
 	}
