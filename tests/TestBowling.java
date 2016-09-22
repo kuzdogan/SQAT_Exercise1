1b3217b02f9aaa8f5925831ebff8dc8cda0cc582
somename <someemail>
2016-09-22 15:53:43 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -80,9 +80,7 @@ public class TestBowling {
 		game.addFrame(frame);
 		game.addFrame(frame);
 		game.addFrame(frame);
-		game.addFrame(frame);
-		game.addFrame(frame);
-		game.addFrame(frame);
+
 
 		
 		// Excessive frame.
