ad9c195532a6bd74e77f8ab96303b6ca0538afa6
somename <someemail>
2016-09-22 16:38:37 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -9,7 +9,9 @@ public class BowlingGame {
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
 	
-	public BowlingGame(){}
+	public BowlingGame(){
+		bonus = new Frame(0,0)
+	}
 	
 	// adds a frame to the game
 	public void addFrame(Frame frame) throws BowlingException{
