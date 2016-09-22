16e44a2214c4cc34f9a28c2cb5bc0b3dc56519c4
somename <someemail>
2016-09-22 16:17:09 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -164,7 +164,7 @@ public class TestBowling {
 		game.addFrame(new Frame(10, 0));
 		game.addFrame(new Frame(3, 4));
 		
-		assertEquals(25, game.score());
+		assertEquals(32, game.score());
 	}
 	@Test
 	public void testGameScore4FrameWithSpare() throws BowlingException {
